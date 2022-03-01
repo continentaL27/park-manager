509be2a722444698d775d2dfa24284e1338722b6
Dima Gorbatenko <dgorbatenko.qb@gmail.com>
2022-03-02 02:41:05 +0300

    add hello endpoint

--- src/main/java/com/conti/prkmngr/PrkmngrApplication.java
+++ src/main/java/com/conti/prkmngr/PrkmngrApplication.java
@@ -1,9 +1,18 @@
 package com.conti.prkmngr;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.jdbc.*;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class,
+    SecurityAutoConfiguration.class
+})
 public class PrkmngrApplication {
 
 	public static void main(String[] args) {
