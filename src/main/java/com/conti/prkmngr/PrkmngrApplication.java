a89e6c3da72be814ff2f296ddfa04780eaa2b125
Dima Gorbatenko <dgorbatenko.qb@gmail.com>
2022-03-02 01:57:19 +0300

    initial commit

---
+++ src/main/java/com/conti/prkmngr/PrkmngrApplication.java
@@ -0,0 +1,13 @@
+package com.conti.prkmngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrkmngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrkmngrApplication.class, args);
+	}
+
+}
